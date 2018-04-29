916ef99a31dc45b0cd3977855e41262a6861d8f3
Puneet Tokhi <puneettokhi25@gmail.com>
2018-04-29 00:09:54 -0700

    Added AddPartyViewController

---
+++ Party.swift
@@ -0,0 +1,15 @@
+//
+//  Party.swift
+//  Login
+//
+//  Created by Dominic on 4/17/18.
+//  Copyright © 2018 CMPE 137. All rights reserved.
+//
+
+import Foundation
+
+struct Party{
+    let type:String
+    let belong:[String]
+    
+}
