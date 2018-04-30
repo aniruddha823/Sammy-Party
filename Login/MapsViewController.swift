7404599138181d17e83cd06de5b58b481069b7a0
Dominic <dominic.varakukala@gmail.com>
2018-04-29 18:19:30 -0700

    Update MapsViewController.swift

--- Login/MapsViewController.swift
+++ Login/MapsViewController.swift
@@ -10,18 +10,24 @@ import UIKit
 import MapKit
 
 class MapsViewController: UIViewController {
+    //Mapview instance
     @IBOutlet weak var myMapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //create search request
         let searchRequest = MKLocalSearchRequest()
+        //temperorary query
         searchRequest.naturalLanguageQuery = "One Washington Square San Jose, CA 95192"
+        
         let activitySearch = MKLocalSearch(request: searchRequest)
+        //Search the string
         activitySearch.start { (response, error) in
             if(response == nil){
                 print("error")
             }
             else{
+                //create pin annotation
                 let annotations = self.myMapView.annotations
                 self.myMapView.removeAnnotations(annotations)
                 
@@ -30,13 +36,15 @@ class MapsViewController: UIViewController {
                 let longitude = response?.boundingRegion.center.longitude
                 
                 let annotation = MKPointAnnotation()
+                //sjsu is pinned anotation
                 annotation.title = "SJSU"
                 annotation.coordinate = CLLocationCoordinate2DMake(latitude!, longitude!)
                 self.myMapView.addAnnotation(annotation)
-                
+                //set coordinate
                 let coordinate:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude!,longitude!)
                 let span = MKCoordinateSpanMake(0.01, 0.01)
                 let region = MKCoordinateRegionMake(coordinate, span)
+                //display map
                 self.myMapView.setRegion(region, animated: true)
                 
             }
